0ee0069031fd45bfdc215a877d7132f50ab4492c
BT_2S14_25 <andyfang1967@gmail.com>
2025-01-08 11:00:20 -0500

    TH-1 : Warned the player if the treasure has already been found. Added a discard option to the menu and the user may be able to type the treasure they want to discard and the treasure kit would delete the specific inputted treasure from the treasure kit.

--- src/Hunter.java
+++ src/Hunter.java
@@ -13,7 +13,7 @@ public class Hunter
     //instance variables
     private String hunterName;
     private String kit;
-    private String treasureKit;
+    private String treasureKit = "Treasure Found: ";
     private int gold;
 
     //Constructor
@@ -78,54 +78,54 @@ public class Hunter
     // Our TreasureKit List
 
     public boolean addTreasure(String treasure){
-        if(addTreasureKit(treasure)){
-            return true;
-        }
-        return false;
-    }
-
-    private boolean addTreasureKit( String treasure){
-        treasureKit += treasure;
+        boolean contains = false;
 
         if(hasTreasure(treasure)){
-            boolean inTreasureKit = true;
+            contains = true;
         }
 
+        treasureKit += treasure + " ";
+
+        return contains;
     }
+
 
     public boolean hasTreasure(String item)
     {
-        int placeholder = 0;
-
-        while (placeholder < treasureKit.length() - 1)
-        {
-            int endOfItem = treasureKit.indexOf(TREASUREKIT_DELIMITER, placeholder);
-            String tmpItem = treasureKit.substring(placeholder, endOfItem);
-            placeholder = endOfItem + 1;
-            if (tmpItem.equals(item))
-            {
-                // early return
-                return true;
-            }
+        if(treasureKit.contains(item) ){
+            return true;
         }
+
         return false;
     }
 
     public void removeTreasureFromKit(String item)
     {
-        int itmIdx = kit.indexOf(item);
+        int itmIdx = treasureKit.indexOf(item);
 
         // if item is found
         if (itmIdx >= 0)
         {
-            String tmpKit = kit.substring(0, itmIdx);
-            int endIdx = kit.indexOf(KIT_DELIMITER, itmIdx);
-            tmpKit += kit.substring(endIdx + 1);
+            String tmpKit = treasureKit.substring(0, itmIdx);
+            tmpKit = tmpKit + treasureKit.length();
+            tmpKit += treasureKit.substring(itmIdx + 1);
 
             // update kit
-            kit = tmpKit;
+            treasureKit = tmpKit;
         }
     }
+
+    public String getTreasureInventory(){
+        String regex = " ";
+        String arr[] = treasureKit.split(regex);
+        String fullList =  "";
+        for(int i =0; i < arr.length; i++) {
+            fullList += arr[i] + " ";
+        }
+        return fullList;
+
+    }
+
 
 
     /**
