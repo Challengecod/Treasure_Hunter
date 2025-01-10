a5c336b76a2f832d4c66fad9b9654d8f1454f60e
BT_2S14_25 <andyfang1967@gmail.com>
2025-01-10 10:22:32 -0500

    TH-5 : Made sure the user gets back gold as long as its within two distance away.

--- src/Casino.java
+++ src/Casino.java
@@ -22,7 +22,7 @@ public class Casino {
             printMessage += "\nNice you guessed the lucky roll!\n You get double the gold back!" + "\n Gold amount: " + customer.getGold();
         }
 
-        else if (rollNumber - wagerGold == 2 || rollNumber - wagerGold == -2) {
+        else if (rollNumber - wagerGold == 1 || rollNumber - wagerGold == 2 || rollNumber - wagerGold == -2 || rollNumber - wagerGold == -1) {
             customer.changeGold(wagerGold);
             printMessage += "\nTwo away from the roll and so you get all of the gold you wagered back!" +
                     "\nGold: " + customer.getGold();
@@ -30,7 +30,7 @@ public class Casino {
         }
         else{
 
-            printMessage += "\nUnlucky, you lose " + wagerGold + "\nCome back next time!";
+            printMessage += "\nUnlucky, you lose " + wagerGold + "gold \nCome back next time!";
         }
     }
 
